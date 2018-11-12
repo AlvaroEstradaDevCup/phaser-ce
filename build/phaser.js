d2fd69d79ccc136117b8def69d4dff162f215f42
Alvaro Estrada <alvaro.estrada@dev-cup.com>
2018-11-12 15:37:34 -0600

    Changed the vertexAttribPointer in the main file

--- build/phaser.js
+++ build/phaser.js
@@ -20178,7 +20178,9 @@ PIXI.WebGLSpriteBatch.prototype.flush = function ()
         gl.vertexAttribPointer(shader.colorAttribute, 4, gl.UNSIGNED_BYTE, true, stride, 16);
 
         // Texture index
+        if (shader.aTextureIndex > -1) {
         gl.vertexAttribPointer(shader.aTextureIndex, 1, gl.FLOAT, false, stride, 20);
+        }
     }
 
     // upload the verts to the buffer
